7093aebf90ab5898ac0922b7051bb654756c9178
Nubli <nubli.naim@gmail.com>
2019-07-24 23:32:47 +0900

    footnote

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,9 @@ import Sim from './components/Sim';
 const Wrapper = styled.div`
   padding: 20px;
 `;
+const FootNote = styled.div`
+  font-size: 10px;
+`;
 
 const App: React.FC = () => {
 
@@ -18,6 +21,11 @@ const App: React.FC = () => {
     <Router>
       <Wrapper>
         <Route path="/" component={Sim}/>
+        <FootNote>
+          This is a WIP thing made for fun.
+          <br/>There are probably bugs everywhere, simulation is likely to be inaccurate.
+          <br/>This thing uses the crafting simulator library made by <a href="https://github.com/ffxiv-teamcraft/simulator/" target="_blank" rel="noreferrer">ffxiv-teamcraft</a>
+        </FootNote>
       </Wrapper>
     </Router>
   );
